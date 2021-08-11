92ddf807cf808b652f64c07b08f5ba6de0537fe1
Tin <25923486+scsfdev@users.noreply.github.com>
2021-08-11 11:35:07 +0800

    Add project files.

---
+++ UR21_DualControllers_Demo/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using UR21_DualControllers_Demo.ViewModel;
+
+namespace UR21_DualControllers_Demo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the MainWindow class.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+            Closing += (s, e) => ViewModelLocator.Cleanup();
+        }
+    }
+}
---
+++ UR21_DualControllers_Demo/Model/DataItem.cs
@@ -0,0 +1,16 @@
+﻿namespace UR21_DualControllers_Demo.Model
+{
+    public class DataItem
+    {
+        public string Title
+        {
+            get;
+            private set;
+        }
+
+        public DataItem(string title)
+        {
+            Title = title;
+        }
+    }
+}
